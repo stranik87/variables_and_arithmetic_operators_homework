e9acf6cbfa62cb21e199e35b7857c35b953d7e21
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-18 17:24:58 +0300

    chang08

--- arithmetic_op08.py
+++ arithmetic_op08.py
@@ -5,4 +5,4 @@ b = 4
 # Create a variable "c"  and assign it the value of the int 2.
 c = 2
 # Create a variable called 'answer' and assign it the expression value: a*b:c
-answer = a * b / c
+answer  = a * b / c
