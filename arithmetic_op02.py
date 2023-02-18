1be583fcff039634952c4b3afa9fe74113df9f21
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-18 17:21:34 +0300

    chang02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -4,6 +4,6 @@ a = 3
 # Create a variable "b"  and assign it the value of the int 4.
 b = 4
 # Create a variable called 'answer' and assign it the sum of a and b.
-answer = a + b
+answer =  a + b
  
 print(answer)
