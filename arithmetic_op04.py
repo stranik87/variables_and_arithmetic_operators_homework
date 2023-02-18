2a29828642f964350b245ec37d92214c0ba2fa8e
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-18 17:22:52 +0300

    chang04

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -2,7 +2,7 @@
 a = 5
 
 # Create a variable "b"  and assign it the value of the int 4.
-b = 4
+b =  4
 
 # Create a variable called 'answer' and assign it the multiplication of a and b.
 answer = a * b
