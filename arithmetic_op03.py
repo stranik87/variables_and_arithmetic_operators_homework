4f2fc1c6bc620b4579d04c19b659c63f27a49dfe
stranik87 <tulkin.boynazarov@gmail.com>
2023-02-18 17:22:18 +0300

    cang03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -2,7 +2,7 @@
 a = 6
 
 # Create a variable "b"  and assign it the value of the int 2.
-b = 2
+b =  2
 
 # Create a variable called 'answer' and assign it the difference of a and b.
 answer = a - b
